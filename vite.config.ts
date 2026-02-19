cb814ac887c8bffc16660edee0b92954c8f25c54
coldestconcept <recognizemiracles@gmail.com>
2026-02-18 21:56:45 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
@@ -21,3 +25,6 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
+
+})
